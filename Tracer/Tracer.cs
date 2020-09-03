b18acdf47aac608da27464716b936983a8ff6b44
Anton Pashkevich <mario.lazer@mail.ru>
2020-09-03 12:38:11 +0300

    solution initialization done

---
+++ Tracer/Tracer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class Tracer : ITracer
+    {
+        public object GetTraceResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTrace()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
